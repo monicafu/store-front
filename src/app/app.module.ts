dd132e71f7fbef6ee207ee83f0884dd6a4744a6e
cse512assignment <yangyuhao830@yahoo.com>
2018-01-02 23:24:19 -0800

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { routing } from './app.routing';
 
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    MatButtonModule,
+    MatCheckboxModule,
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
