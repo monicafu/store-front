5b71433f1721f4be122cb2df8f4ef2c93bb767a7
monicafu <31578677+monicafu@users.noreply.github.com>
2018-01-21 22:40:49 -0800

    add nav-bar and css style and upadate app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { routing } from './app.routing';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    routing,
+    HttpModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
