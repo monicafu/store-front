48cf8e3cdbe643e181563d5dd76f94fcd3a73e7d
monicafu <31578677+monicafu@users.noreply.github.com>
2018-02-27 16:50:31 -0800

    fixed sytax error on home page and the page is load successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,15 @@ import { routing } from './app.routing';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import 'hammerjs';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-=======
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
->>>>>>> dd132e71f7fbef6ee207ee83f0884dd6a4744a6e
+
 
 
 @NgModule({
@@ -30,21 +24,13 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     FormsModule,
     MatButtonModule,
     MatCheckboxModule,
     routing,
     HttpModule,
     HttpClientModule,
-=======
-    routing,
-    MatButtonModule,
-    MatCheckboxModule,
-    HttpModule,
-    FormsModule,
     BrowserAnimationsModule
->>>>>>> dd132e71f7fbef6ee207ee83f0884dd6a4744a6e
   ],
   providers: [],
   bootstrap: [AppComponent]
