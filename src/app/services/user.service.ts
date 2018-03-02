39fe29cdb1e6e23efde3b75b904406b4224328aa
monicafu <31578677+monicafu@users.noreply.github.com>
2018-03-02 15:41:34 -0800

    Added payment service side

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -57,19 +57,6 @@ export class UserService {
     return this.http.post(url, JSON.stringify(userInfo), {headers:tokenHeader});
   }
 
-  retrievePassword(email:string) {
-    let url = this.serverPath+'/user/forgetPassword';
-    let userInfo = {
-      "email" : email
-    }
-    let tokenHeader = new Headers({
-      'Content-Type' : 'application/json',
-      'x-auth-token' : localStorage.getItem('xAuthToken')
-    });
-
-    return this.http.post(url, JSON.stringify(userInfo), {headers : tokenHeader});
-  }
-
   getCurrentUser() {
     let url = this.serverPath+'/user/getCurrentUser';
 
