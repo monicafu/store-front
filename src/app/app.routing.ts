ecb678867f4fc6d39ae34846a6323fdfbe1fbec7
monicafu <31578677+monicafu@users.noreply.github.com>
2018-03-29 21:58:29 -0700

    Added order-summary component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,15 @@
-import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {MyAccountComponent} from './components/my-account/my-account.component';
-import {MyprofileComponent} from "./components/myprofile/myprofile.component";
-import {BookListComponent} from './components/book-list/book-list.component';
-import {BookDetailComponent} from "./components/book-detail/book-detail.component";
-import {ShoppingCartComponent} from "./components/shopping-cart/shopping-cart.component";
-import {OrderComponent} from "./components/order/order.component";
+import { ModuleWithProviders} from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+import { HomeComponent} from './components/home/home.component';
+import { MyAccountComponent} from './components/my-account/my-account.component';
+import { MyprofileComponent} from "./components/myprofile/myprofile.component";
+import { BookListComponent} from './components/book-list/book-list.component';
+import { BookDetailComponent} from "./components/book-detail/book-detail.component";
+import { ShoppingCartComponent} from "./components/shopping-cart/shopping-cart.component";
+import { OrderComponent} from "./components/order/order.component";
+import { OrderSummaryComponent} from './components/order-summary/order-summary.component';
+
 
 const appRoutes: Routes = [
 	{
@@ -41,6 +44,10 @@ const appRoutes: Routes = [
   {
     path : 'checkout',
     component : OrderComponent
+  },
+  {
+    path: 'orderSummary',
+    component: OrderSummaryComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
